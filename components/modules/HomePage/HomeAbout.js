2ab3222d987a195f6fe46b162d6106184696e018
mohammad <mohammad@gmail.com>
2024-07-18 00:26:25 +0330

    about us page finished

--- components/modules/HomePage/HomeAbout.js
+++ components/modules/HomePage/HomeAbout.js
@@ -1,69 +1,81 @@
+import Image from 'next/image';
 import React from 'react';
+import Call from '../../templates/Call';
 
 const HomeAbout = () => {
   return (
-    <section className="about-section py-3 p-lg-4 bg-gray-850 border-gray-800 section-block aos-init aos-animate" id="about-section" data-aos="fade-up">
+    <section className="about-section my-3 py-3 p-lg-4 border-gray-800 section-block aos-init aos-animate" id="about-section" data-aos="fade-up">
       <div className="container">
-        <div className="row flex-xl-row-reverse">
-        <div className="col-xl-6 image-block aos-init aos-animate" data-aos="fade-right" data-aos-delay="200" data-aos-duration="2000">
-            <div className="img-wrapper about-img-wrap" data-tilt="" data-tilt-max="10" style={{ transform: "perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)", willChange: "transform" }}>
-              {/* <img className="about-img-1 rounded img-fluid" src="/images/IMG_۲۰۲۳۰۸۲۷_۲۲۲۰۱۷.jpg" alt="about image" /> */}
-              {/* <img className="about-img-2 img-fluid" src="image/about-image-2.png" alt="about image 2" /> */}
-            </div>
+        <div className="row flex-md-row-reverse justify-content-center justify-content-lg-between align-items-center">
+        <div className="col-md-5 d-none d-md-block">
+          <Image className='img-fluid border rounded' src={"/images/about-me.png"} width={1080} height={1350} />
           </div>
-          <div className="col-xl-6 content-block text-end aos-init aos-animate" data-aos="fade-right" data-aos-delay="400" data-aos-duration="2000">
-            <div style={{fontFamily:"Vazirmatn"}} className='d-flex flex-column align-items-center align-items-lg-start'>
-                <span className='color-org-100 fs-5 mt-3 mt-lg-0'>
+          <div className="col-md-6 content-block text-end aos-init aos-animate" data-aos="fade-right" data-aos-delay="400" data-aos-duration="2000">
+            <div className='d-flex flex-column align-items-center align-items-lg-start'>
+                <span style={{fontFamily:"KalamehWeb-Bold" , color:"#090736" , fontSize:"20px"}} className='mb-4 mt-4 mt-lg-0'>
                     درباره من
                 </span>
-                <span className='fw-bold color-linear fs-2 py-3 mb-2 mt-4'>
+                <span style={{fontFamily:"KalamehWeb-Bold" , color:"#090736" , fontSize:"30px"}} className='fw-bold pt-3 mb-3'>
                     امیرحسین مبشری
                 </span>
-                <span className='fw-bold color-linear fs-3 py-3 mb-4'>
+                <span style={{fontFamily:"KalamehWeb-Bold" , color:"#090736" , fontSize:"25px"}}  className='fw-bold color-linear py-3 mb-4'>
                     مدرس تخصصی زبان انگلیسی
                 </span>
             </div>
-            <p className='homeabout-intro text-center text-lg-end' style={{lineHeight:"27px" , fontSize:"15px"}}>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+            <Image className='img-fluid border rounded border d-md-none' src={"/images/about-me.png"} width={1080} height={1350} />
+            <p className='text-start text-black fw-bold mb-2' style={{lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+            Hey!
             </p>
-            <p className='homeabout-intro text-center text-lg-end mt-3' style={{lineHeight:"27px" , fontSize:"15px"}}>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+            <p className='text-start text-black fw-bold border-bottom py-2' style={{direction:"ltr", lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+            My name is AmirHossein Mobasheri and I hold a teacher training course (TTC) certification as well as a prestigious &quot;Leaders of Learning&quot; certificate from Harvard University.
             </p>
-            <div className="personal-details row mb-3">
-              <div className="col-md-6">
-                <ul className="personal-info text-center text-lg-end">
-                  <li>
-                    <h4>نام و نام خانوادگی</h4>
-                    <p>امیرحسین مبشری</p>
-                  </li>
-                  <li>
-                    <h4>ایمیل</h4>
-                    <p>example@gmail.com</p>
-                  </li>
-                  <li>
-                    <h4>سن</h4>
-                    <p>23 سال</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6">
-                <ul className="personal-info text-center text-lg-end">
-                  <li>
-                    <h4>تحصیلات</h4>
-                    <p>دانشجوی کارشناسی علوم تربیتی</p>
-                  </li>
-                  <li>
-                    <h4>شغل</h4>
-                    <p>دبیر</p>
-                  </li>
-                </ul>
-              </div>
+            <p className='text-start text-black fw-bold border-bottom py-2' style={{direction:"ltr", lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+            Additionally, I&apos;ve earned a master&apos;s degree in Educational Science, providing me with a deep understanding of the learning process.
+            </p>
+            <p className='text-start text-black fw-bold border-bottom py-2' style={{direction:"ltr", lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+            As a content creator, I&apos;ve dedicated my time to teaching English for free on both my Instagram and YouTube channels.
+            </p>            
+            <p className='text-start text-black fw-bold border-bottom py-2' style={{direction:"ltr", lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+            My goal is to make learning English fun, engaging, and accessible for everyone.
+           </p>
+           <p className='text-start text-black fw-bold border-bottom py-2' style={{direction:"ltr", lineHeight:"27px" , fontFamily:"Arial" , fontSize:"15px"}}>
+           Join me on this exciting journey of learning and growing together.
+           </p>
+            {/* <div className='text-center mb-3 text-lg-end'>
+                <a className="btn-org-1 text-white rounded border px-3 py-2" href="" download >دانلود رزومه</a>
+            </div> */}
+          </div>
+          <div className='col-12'>
+          <p className='text-end text-md-center text-black fw-bold mb-2 border-bottom py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px"}}>
+            سلام ,من امیرحسین مبشری هستم.
+            </p>
+            <p className='text-end text-md-center text-black fw-bold mb-2 border-bottom py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px"}}>
+            دارای مدرک دوره تربیت معلم (TTC) و همچنین مدرک معتبر «رهبران یادگیری» از دانشگاه هاروارد.
+            </p>
+            <div className='text-center'>
+            <Image style={{width:"300px"}} className='rounded border img-fluid' src={"/images/TTC.jpg"} width={2480} height={3508} />
+            <p className='text-center text-black fw-bold mb-2 py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px" , direction:"ltr"}}>
+              TTC certificate
+            </p>
             </div>
-            <div className='text-center mb-3 text-lg-end'>
-                <a className="btn-org-1 text-white rounded px-3 py-2" href="" download >دانلود رزومه</a>
+            <div className='text-end text-md-center border-bottom'>
+            <Image style={{width:"300px"}} className='rounded img-fluid' src={"/images/Certificate_page-0001.jpg"} width={2480} height={3508} />
+            <p className='text-center text-black fw-bold pb-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px" , direction:"ltr"}}>
+              GSEx2 certificate
+            </p>
             </div>
+            <p className='text-end text-md-center text-black fw-bold mb-2 border-bottom py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px"}}>
+            مدرک کارشناسی در رشته علوم تربیتی نیز به درک عمیق من از فرآیند یادگیری زبان آموزان کمک میکند.
+            </p>
+            <p className='text-end text-md-center text-black fw-bold mb-2 border-bottom py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px"}}>
+            به‌عنوان یک تولیدکننده محتوا، بخشی از زمان خود را صرف آموزش رایگان زبان انگلیسی در اینستاگرام و یوتیوب کرده ام و تلاش میکنم تا یادگیری این زبان را برای همه، سرگرم کننده، جذاب و قابل دسترس کنم.
+            </p>
+            <p className='text-end text-md-center text-black fw-bold mb-2 border-bottom py-2' style={{lineHeight:"27px" , fontFamily:"KalamehWeb-Medium" , fontSize:"15px"}}>
+            از شما دعوت میکنم تا در این مسیر شگفت انگیز, با من همراه شوید!
+            </p>
           </div>
         </div>
+        <Call />
       </div>
     </section>
   );
--- components/templates/Call.js
+++ components/templates/Call.js
@@ -4,35 +4,24 @@ import ContactForm from '../modules/call/ContactForm';
 const Call = ({form , setForm}) => {
     return (
         <div className='px-4 px-lg-0'>
-        <div className="text-center ff-vazir pt-4">
-          <h2 style={{width:"fit-content"}} className='fs-1 d-flex justify-content-center my-3 py-1 mx-auto color-linear'>
-            تماس با ما
-          </h2>
-          
-          <p className="lead mb-4">
-            سلام دوستان، من امیرحسین مبشری هستم و اینجا هم سایت شخصی منه که در اصل برای شماست :)
-            <br />
-             من در اینجا ضمن معرفی خودم، آموخته هام از زبان انگلیسی رو باهاتون به اشتراک می‌ذارم
-          </p>
-        </div>
-        <h4 className='color-linear text-center pt-4'>راه های ارتباطی</h4>
-        <div className='d-flex flex-column flex-lg-row align-items-cenrer justify-content-center pt-4 mb-4 mb-lg-0'>
+        <h4 style={{fontFamily:"KalamehWeb-Bold" , fontSize:"30px" , color:"#090736"}} className='text-center pt-3 pb-lg-2'>راه های ارتباطی</h4>
+        <div className='d-flex flex-column flex-lg-row align-items-cenrer justify-content-center pt-4 mb-0 mb-lg-0'>
             <div className='d-flex justify-content-center mx-lg-3 my-3 my-lg-0'>
-            <a rel='noreferrer' href='' className='social-link d-flex align-items-center'>
+            <a rel='noreferrer' href='https://instagram.com/english_with_mobash?igshid=MzNlNGNkZWQ4Mg==' className='social-link d-flex align-items-center'>
                     <span>instagram</span>
                     <i class="bi bi-instagram me-1"></i>
                 </a>
             </div>
             <div className='d-flex justify-content-center mx-lg-3 my-3 my-lg-0'>
-            <a rel='noreferrer' href='mailto:' className='social-link d-flex align-items-center'>
-                    <span>Email</span>
-                    <i class="bi bi-envelope-fill me-1"></i>
+            <a rel='noreferrer' href='https://www.youtube.com/@English.with.Mobash' className='social-link d-flex align-items-center'>
+                    <span>Youtube</span>
+                    <i class="bi bi-youtube me-1"></i>
                 </a>
             </div>
             <div className='d-flex justify-content-center mx-lg-3 my-3 my-lg-0'>
-                <a rel='noreferrer' href='' className='social-link d-flex align-items-center'>
-                    <span>linkedin</span>
-                    <i class="bi bi-linkedin me-1"></i>
+                <a rel='noreferrer' href='https://api.whatsapp.com/send?phone=989383439244' className='social-link d-flex align-items-center'>
+                    <span>Whatsapp</span>
+                    <i class="bi bi-whatsapp me-1"></i>
                 </a>
             </div>
         </div>
